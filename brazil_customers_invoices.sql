ceb5b86dce8b22eb3334934fe04757e65d4d90cb
alambrow <alexander.lambrow@gmail.com>
2021-08-23 11:24:38 -0500

    made change to brazil_customers_invoices

--- brazil_customers_invoices.sql
+++ brazil_customers_invoices.sql
@@ -2,3 +2,8 @@ SELECT Invoice.InvoiceId, Invoice.InvoiceDate, Invoice.BillingCountry, Customer.
 FROM Invoice
 JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
 WHERE Invoice.BillingCountry='Brazil';
+
+SELECT Invoice.InvoiceId, Invoice.InvoiceDate, Invoice.BillingCountry, Customer.FirstName || '' || Customer.LastName AS FullName
+FROM Invoice
+JOIN Customer ON Invoice.CustomerId = Customer.CustomerId
+WHERE Invoice.BillingCountry='Brazil';
